8439f0bdb77cbe34e7d2629f5a9017356ac322b9
nemliProg <younesbuzz@gmail.com>
2022-12-17 15:01:30 +0100

    add Home Controller

---
+++ src/main/java/com/springjwt/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.springjwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to Spring JWT";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "Welcome to Spring JWT Home Page";
+    }
+
+
+}
